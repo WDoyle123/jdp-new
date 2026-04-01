954b5b6993c0968b1c231ec5facc6333a6a9af24
William Doyle <will.doyle30@gmail.com>
2026-04-01 20:40:33 +0100

    init commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-10/12 bg-olive h-24 mt-8 rounded-xl"></div>
+    </div>
+  );
+}
+
+export default NavBar;
